045aa69bfccb066ec2d874ddca738c64a3fc3593
Keith Roberts <krober10@nd.edu>
2019-10-30 00:16:13 -0300

    More clean up

--- fastHJ.cpp
+++ fastHJ.cpp
@@ -1,4 +1,5 @@
 /* solve the Hamilton-Jacobi equation to smooth a raster field 
+ * ref: Per Olof, 
 / kjr, usp, 2019
 */
 #include "fastHJ.H"
@@ -16,7 +17,13 @@
 #define EPS 1e-9
 
 
-// find index in linear time 
+// Function to check whether
+// the element is odd or not.
+bool IsOdd(int i)
+{
+    return ((i % 2) == 1);
+}
+// find indices in linear time where A==value
 std::vector<int> findIndices(const std::vector<int>& A, const int value) {
 	// try calling reserve here with some estimated size amount
 	std::vector<int> B; 
@@ -27,7 +34,6 @@ std::vector<int> findIndices(const std::vector<int>& A, const int value) {
 	}
 	return B; 
 }
-
 // solve the Hamilton-Jacobi equation
 std::vector<double> limgrad(const std::vector<int>& dims, const double elen, const std::vector<double> &ffun, const double dfdx, const int imax)  {
  
@@ -47,9 +53,11 @@ std::vector<double> limgrad(const std::vector<int>& dims, const double elen, con
            //------------------------- find "active" nodes this pass
            auto aidx = findIndices( aset, iter); 
 
+           //------------------------- convergence
 	       if(aidx.empty()) break; 
 
 	       for(std::size_t i; i < aidx.size(); i++) {
+
               //----- map doubly index to singly indexed 
 	          int inod = aidx[i];
 	          int ipos = 1 + std::floor((inod-1)/dims[2]);
@@ -61,7 +69,38 @@ std::vector<double> limgrad(const std::vector<int>& dims, const double elen, con
               npos[2] = (ipos-2)*ny + jpos;//nnod of left adj
               npos[3] = (ipos-1)*ny + std::min(jpos+1,ny);//nnod of above adj
               npos[4] = (ipos-1)*ny + std::max(jpos-1,1);//nnod of below adj
-	       };
+
+              //----- handle boundary vertex adjs.
+              //----- iterator that stores the position of last element 
+              std :: vector <int>::iterator pend; 
+              pend = std::remove_if (npos.begin(),npos.end(), IsOdd);  
+
+              for(std::vector<int>::iterator p=npos.begin(); p != pend; ++p){
+
+                 int nod1 = npos[0];
+                 int nod2 = *p; 
+
+                 //----------------- calc. limits about min.-value
+                 if (ffun[nod1] > ffun[nod2]) {
+
+                     fun1 = ffun[nod2] + elen * dfdx ;
+
+                     if (ffun[nod1] > fun1+ftol) {
+                         ffun[nod1] = fun1;
+                         aset[nod1] = iter;
+                     }
+                 
+                 } else {
+
+                     fun2 = ffun[nod1] + elen * dfdx ;
+
+                     if (ffun[nod2] > fun2+ftol) {
+                         ffun[nod2] = fun2;
+                         aset[nod2] = iter;
+                     }
+                 }
+              }
+	       }
 
 	}
 	return ffun; 
@@ -73,19 +112,19 @@ int main() {
 
     std::vector<int> dims = {10,10,10};
     double elen = 1.0;
-    std::vector<double> ffun[1000]; 
-    ffun.fill(0); 
+    std::vector<double> ffun;
+    ffun.reserve(1000);
+    std::fill(ffun.begin(), ffun.end(),0);
     double dfdx=0.15; 
-    int imax=10;
+    int imax=std::sqrt(10000000);
      
-    auto start = std::chrono::high_resolution_clock::now();
+    auto begin = std::chrono::steady_clock::now();
 
-    auto ffun_s  = limgrad( dims, elen, ffun, dfdx, imax);
+    std::vector<double> ffun_s  = limgrad( dims, elen, ffun, dfdx, imax);
 
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    auto end = std::chrono::steady_clock::now();
 
-    std::cout << "Method took " << std::chrono::duration.count() << " microseconds\n";
+    std::cout << "Method took " << std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count() << " microseconds\n";
 
     return 0; 
 }
--- fastHJ.h
+++ fastHJ.h
@@ -8,6 +8,8 @@ std::vector<int> findIndices(const std::vector<int>& A, const int value);
 
 std::vector<double> limgrad( const std::array<int,3> &dims, const double elen, const std::vector<double> &ffun, const double dfdx, const int imax); 
 
+
+
 #endif 
  
 
