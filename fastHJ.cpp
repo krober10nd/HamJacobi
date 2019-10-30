a08ba731c51b74bb355062e6864bb8b60f38a2df
Keith Roberts <krober10@nd.edu>
2019-10-29 22:25:33 -0300

    Cleaning up bugs

--- fastHJ.cpp
+++ fastHJ.cpp
@@ -3,10 +3,15 @@
 */
 #include "fastHJ.H"
 
+#include <iostream>
 #include <stdio.h>
 #include <vector> 
 #include <array>
 #include <math.h>
+#include <cmath>
+
+#include <chrono>
+
 
 #define EPS 1e-9
 
@@ -24,36 +29,39 @@ std::vector<int> findIndices(const std::vector<int>& A, const int value) {
 }
 
 // solve the Hamilton-Jacobi equation
-std::vector<double> ffun limgrad( const std::vector<int>& dims, const double elen, const std::vector<double> &ffun, const double dfdx, const int imax)  {
+std::vector<double> limgrad(const std::vector<int>& dims, const double elen, const std::vector<double> &ffun, const double dfdx, const int imax)  {
  
-	int ny = dims[0]; int nx = dims[1]; int nz = dims[2];
+	    int ny = dims[0]; int nx = dims[1]; int nz = dims[2];
 
-        std::array<int,ny*nz*nx> aset;
-        aset.fill(0);
+        std::vector<int> aset(ny*nx*nz,0);
 
-        double ftol = std::min_element(ffun.begin(), ffun.end())*std::sqrt(EPS);
+        double ftol = *(std::min_element(ffun.begin(), ffun.end()))*std::sqrt(EPS);
 
-        auto rm = array<double,5>;
-        rm.fill(0);
+        std::array<int,5> rm;
+        rm.fill(0); 
+        std::array<int,5> npos;
+        npos.fill(0); 
 
-        for(int iter=0; iter < itmax; iter++) {
+        for(int iter=0; iter < imax; iter++) {
 
            //------------------------- find "active" nodes this pass
            auto aidx = findIndices( aset, iter); 
 
-	   if(aidx.empty) break; 
+	       if(aidx.empty()) break; 
 
-	   for(std:size_t :: i; i < aidx.size(); i++) {
-	      
+	       for(std::size_t i; i < aidx.size(); i++) {
               //----- map doubly index to singly indexed 
-	      int inod = aidx[i];
-	      int ipos = 1 + std::floor((inod-1)/dims[3];
-              int jpos = inod - (ipos - 1)*dims[3];  
-
-	      // ---- gather indices using 4 edge stencil 
-	      //
-	      //
-	   };
+	          int inod = aidx[i];
+	          int ipos = 1 + std::floor((inod-1)/dims[2]);
+              int jpos = inod - (ipos - 1)*dims[2];  
+    
+	          // ---- gather indices using 4 edge stencil 
+              npos[0] = inod; 
+              npos[1] = ipos*ny + jpos;// 
+              npos[2] = (ipos-2)*ny + jpos;//nnod of left adj
+              npos[3] = (ipos-1)*ny + std::min(jpos+1,ny);//nnod of above adj
+              npos[4] = (ipos-1)*ny + std::max(jpos-1,1);//nnod of below adj
+	       };
 
 	}
 	return ffun; 
@@ -61,8 +69,23 @@ std::vector<double> ffun limgrad( const std::vector<int>& dims, const double ele
 
 
 //
-int main {
-   
+int main() {
 
-return 0; 
+    std::vector<int> dims = {10,10,10};
+    double elen = 1.0;
+    std::vector<double> ffun[1000]; 
+    ffun.fill(0); 
+    double dfdx=0.15; 
+    int imax=10;
+     
+    auto start = std::chrono::high_resolution_clock::now();
+
+    auto ffun_s  = limgrad( dims, elen, ffun, dfdx, imax);
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << "Method took " << std::chrono::duration.count() << " microseconds\n";
+
+    return 0; 
 }
--- fastHJ.h
+++ fastHJ.h
@@ -4,7 +4,9 @@
 #include <vector> 
 #include <array> 
 
-double* ffun limgrad( const std::array<int,3> &dims, const double elen, const std::vector<double> &ffun, const double dfdx, const int imax); 
+std::vector<int> findIndices(const std::vector<int>& A, const int value);
+
+std::vector<double> limgrad( const std::array<int,3> &dims, const double elen, const std::vector<double> &ffun, const double dfdx, const int imax); 
 
 #endif 
  
